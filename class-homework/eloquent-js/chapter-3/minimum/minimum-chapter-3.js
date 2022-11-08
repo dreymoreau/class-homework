733ef0df6db93a3e52750fa4e72167f99975d865
drey <75917821+dreymotoh@users.noreply.github.com>
2022-11-07 20:39:10 -0500

    Add files via upload

---
+++ class-homework/eloquent-js/chapter-3/minimum/minimum-chapter-3.js
@@ -0,0 +1,12 @@
+// Minimum
+// The previous chapter introduced the standard function Math.min that returns its smallest argument. 
+// We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+
+// // Your code here.
+
+
+function returnsSmallestArg(num1, num2) {
+    return Math.min(num1, num2)
+}
+
+console.log(returnsSmallestArg(7, 21))
