711f5f833f223a436d1b697b1ebe9c3abe3f0664
dreymotoh <striclyspoken@gmail.com>
2022-11-16 15:43:07 -0500

    class homework

---
+++ the-js-way-2/the-js-way-2.js
@@ -0,0 +1,48 @@
+// thejsway => 3, 2, 1...Code!
+
+// let age = 25;
+// let name = 'drey'
+
+// console.log(age)
+// console.log(name)
+
+// function nums(num1, num2){
+//     let product;
+
+//     product = num1 * num2 
+//     console.log(product)
+//     product = num1 + num2
+//     console.log(product)
+//     product = num1 - num2
+//     console.log(product)
+//     product = num1 / num2
+//     console.log(product)
+// }
+
+// nums(6,3)
+
+// console.log(4 + 5);
+// console.log("4 + 5");
+// console.log("4" + "5");
+
+// thejsway => Write Functions!
+
+// // Say hello to the user
+// function sayHello(firstName, lastName) {
+//     const message = `Hello, ${firstName} ${lastName}!`;
+//     return message;
+//   }
+  
+//   prompt('hello user, what is your first and last name?')
+//   console.log(sayHello('drey', 'm')) 
+
+//   // TODO: ask user for first and last name
+//   // TODO: call sayHello() and show its result
+
+
+//   // Square the given number x
+// function square1(x) {
+//     x = 5
+//   }
+  
+//   console.log(square1(5, 2))
