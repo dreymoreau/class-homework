a8d79b7cce5062b58bda2815dff33bca9a7195f6
dreymotoh <striclyspoken@gmail.com>
2022-12-07 13:22:56 -0500

    new day!

--- thejsway/c6/c6.js
+++ thejsway/c6/c6.js
@@ -30,3 +30,47 @@ console.log(aurora.describe());
 // Modeling a dog
 
 // Complete the following program to add the dog object definition.
+
+const dog = {
+  name: "Fang",
+  species: "boarhound",
+  size: 75,
+  
+  bark(){
+      return dog.bark = "Grrr! Grrr!"
+  }
+}
+
+// Modeling a circle
+
+// Complete the following program to add the circle object definition. Its radius value is input by the user.
+
+// Modelling a bank account
+
+// A name property set to "Alex".
+// A balance property set to 0.
+// A credit method adding the (positive or negative) value passed as an argument to the account balance.
+// A describe method returning the account description.
+// Use this object to show its description, crediting 250, debiting 80, then show its description again.
+
+
+const account = {
+  name: "Alex",
+  balance: 0,
+
+  credit(value) {
+    return account.balance += value;
+  },
+  
+  describe() {
+    return account;
+  }
+};
+   console.log(`owner: ${account.name}, balance ${account.balance}`)
+  //  output => owner: Alex, balance 0
+
+   account.balance += 250;
+   account.balance -= 80;
+
+  console.log(`owner: ${account.name}, balance ${account.balance}`)
+  //  output => owner: Alex, balance 170
