e4902cc5aca6c4f4cae4c06266a1b44aaef1717e
dreymotoh <striclyspoken@gmail.com>
2022-12-13 10:15:41 -0500

    chapter 1!

---
+++ thejsway/c1/c1.js
@@ -0,0 +1,28 @@
+// Chapter 1: 3, 2, 1...Code!
+
+// Presentation
+// Write a program that displays your name and age. Here's the result for mine.
+
+let name = 'drey'
+let age = 25
+
+console.log(name)
+console.log(age)
+
+// Minimalistic calculator
+// Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3.
+
+function addMultiplySubtract(n1, n2) {
+    console.log(n1 + n2)
+    console.log(n1 - n2)
+    console.log(n1 * n2)
+}
+
+addMultiplySubtract(6,3)
+
+// Values prediction
+// Observe the following program and try to predict the values it displays.
+
+console.log(4 + 5);
+console.log("4 + 5");
+console.log("4" + "5");
