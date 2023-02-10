2baa8bfb6c960ab3ea66b2ac3879adfe2500c249
dreymotoh <striclyspoken@gmail.com>
2023-02-10 09:40:18 -0500

    class homework!

--- js.info/arr-method/arr-methods-task.js
+++ js.info/arr-method/arr-methods-task.js
@@ -73,5 +73,21 @@ function camelize(str) {
     return newString
 }
 
-
 console.log(camelize('one-long-word')) //output => oneLongWord
+
+// Filter Range
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+
+// The function should not modify the array. It should return the new array.
+
+//directly return using filter, compare if item is greater or equals to a or if item is less than or equals to b
+
+let arr = [5, 3, 8, 1];
+
+function filterRange(arr, a, b) {
+    return arr.filter(item => (a <= item && item <= b))
+}
+
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered); // 3,1 (matching values)
+console.log(arr); // 5,3,8,1 (not modified)
